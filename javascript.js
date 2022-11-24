df198c5d55072001f94c983db2223c2972f43531
altramax <ajayi_ezekiel@yahoo.com>
2022-11-24 12:03:58 +0100

    Password Generator

--- javascript.js
+++ javascript.js
@@ -1,24 +1,103 @@
 "use strict"
 // Random Password Generator
 
-let alphabeth = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
- 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
- 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '$', '@', '#', '%', '&', '*'];
+// let alphabeth = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+//  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+//  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '$', '@', '#', '%', '&', '*'];
  
+//  let outPut = document.querySelector('.password');
+
+//   let arr = function (value){
+//     let key = [];
+//     for(let i = 0; i < 12; i++){
+//      key.push(value[Math.floor(Math.random() * value.length) * 1]);
+//     }
+//   return key;
+//   }
+
+// document.querySelector('.click').addEventListener("click", function(){ 
+//   let password = arr(alphabeth);
+//   console.log(password.join(' '));
+//   outPut.textContent = password.join(' ') ;
+//   document.querySelector('.message').textContent = "Your password is"
+// })
+
+
+ 
+  const numbers = [1,2,3,4,5,6,7,8,9,0];
+
+  const smallAlphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+  'n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+  const capitalAlphabets = ['A','B','C','D','E','F','G','H','I','J','K','L',
+  'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+  const symbols = ['@','#','$','%','&','*'];
+
+  let restart = [];
+
+
+document.querySelector('.numbers').addEventListener('change', function(e){
+  if(e.target.checked){
+    for(let i = 0; i < numbers.length; i++){
+      restart.push(numbers[i]);
+    }
+    console.log(restart);
+  }else{ 
+    restart = [];
+    console.log(restart);
+  }
+})
+
+document.querySelector('.small-alphabets').addEventListener('change', function(e){
+  if(e.target.checked){
+    for(let i = 0; i < smallAlphabets.length; i++){
+      restart.push(smallAlphabets[i]);
+    }
+    console.log(restart);
+  }else{ 
+    restart = [];
+    console.log(restart);
+  }
+})
+
+document.querySelector('.capital-alphabets').addEventListener('change', function(e){
+  if(e.target.checked){
+    for(let i = 0; i < capitalAlphabets.length; i++){
+      restart.push(capitalAlphabets[i]);
+    }
+    console.log(restart);
+  }else{ 
+    restart = [];
+    console.log(restart);
+  }
+})
+
+document.querySelector('.symbols').addEventListener('change', function(e){
+  if(e.target.checked){
+    for(let i = 0; i < symbols.length; i++){
+      restart.push(symbols[i]);
+    }
+    console.log(restart);
+  }else{ 
+    restart = [];
+    console.log(restart);
+  }
+})
+
  let outPut = document.querySelector('.password');
 
   let arr = function (value){
     let key = [];
-    for(let i = 0; i < 12; i++){
+    for(let i = 0; i < 4; i++){
      key.push(value[Math.floor(Math.random() * value.length) * 1]);
     }
   return key;
   }
 
 document.querySelector('.click').addEventListener("click", function(){ 
-  let password = arr(alphabeth);
+  let password = arr(restart);
   console.log(password.join(' '));
   outPut.textContent = password.join(' ') ;
   document.querySelector('.message').textContent = "Your password is"
 })
-
