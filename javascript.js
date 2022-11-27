0833ac197a33486f637043bd17456efe608329df
altramax <ajayi_ezekiel@yahoo.com>
2022-11-27 15:48:28 +0100

    Password Generator

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 "use strict"
 // Random Password Generator
-
+  
+//  Method one
 // let alphabeth = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 //  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
 //  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '$', '@', '#', '%', '&', '*'];
@@ -23,7 +24,7 @@
 // })
 
 
- 
+    // method two 
   const numbers = [1,2,3,4,5,6,7,8,9,0];
 
   const smallAlphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
